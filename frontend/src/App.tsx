ebd17c59337a7f76dd418c56311364735f59d814
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-12-05 22:50:14 +0100

    add navbar in app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
+    < Navbar /> 
     < Outlet />
     </>
   )
