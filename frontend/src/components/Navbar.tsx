c8f768d67e2b2b33d7d692a23a918faa695c92b6
BomberQLF <tmurphyparis@gmail.com>
2024-12-06 05:57:58 +0100

    Avancé FRONTEND

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,23 +1,19 @@
 import Logo from '../assets/react.svg'
-import Wave from './Wave'
 
 const Navbar = () => {
     return (
         <>
-        <header className="header-nav" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '1.2rem 2rem 6.5rem 2rem', backgroundColor: '#A3FCFF' }}>
+        <header className="header-nav" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '1.2rem 2rem 6.5rem 2rem' }}>
             <nav>
             <ul className='nav-element' style={{ display: 'flex', listStyle: 'none', padding: 0, margin: 0, gap: '6rem' }}>
-                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center' }}>Home</a></li>
-                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center' }}>About</a></li>
+                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center', color: 'white' }}>Home</a></li>
+                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center', color: 'white'}}>About</a></li>
                 <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px' }}><img src={Logo} style={{ height: '50px' }}></img></a></li>
-                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center' }}>Contact</a></li>
-                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center' }}>Lorem</a></li>
+                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center', color: 'white' }}>Contact</a></li>
+                <li style={{ display: 'flex' }}><a href="#" style={{ textDecoration: 'none', padding: '10px', fontSize: '18px', display: 'flex', alignItems: 'center', color: 'white' }}>Lorem</a></li>
             </ul>
             </nav>
         </header>
-        <div className="wave-container">
-            <Wave style={{ position: 'absolute', rotate: '-.6deg', top: '-53px', zIndex: '100' }}/>
-        </div>
         </>
     );
   };
