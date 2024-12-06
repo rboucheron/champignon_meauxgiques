6cdc29edb4436b7dc74e578b274ebb188376fc76
EliasZerar <zeinzer@yahoo.fr>
2024-12-06 03:52:43 +0100

    Creation quizz

---
+++ frontend/src/components/Accueil.tsx
@@ -0,0 +1,41 @@
+import BullesGauche from '../assets/bullesGauche.svg'
+import Hippocampes from '../assets/hippocampes.png'
+import CorailGauche from '../assets/CorailGauche.png'
+import CorailDroite from '../assets/CorailDroite.png'
+import BigPoisson from '../assets/bigPoisson.png'
+import SmallPoisson from '../assets/smallPoisson.png'
+import SmallBulles from '../assets/smallBulles.png'
+import Plongeur from '../assets/plongeur.png'
+import { Animation } from './animation/Animation'
+import Quizz from './Quizz'
+
+
+const Accueil = () => {
+    return (
+        <section className="accueil">
+            <div className="container">
+                <div className="title-accueil">
+                    <h1 style={{ fontSize: '2.5rem' }}>Bienvenue dans l'eau</h1>
+                    <p>Lalala on est dans l'eau</p>
+                </div>
+                <img src={BullesGauche} id='bullesGauche' style={{ position: 'absolute', left: '60px', top: '350px' }} alt="" />
+                < Animation />
+                <img src={Hippocampes} id='hippocampesDroite' style={{ position: 'absolute', left: '37rem', top: '34rem', height: '60px' }} alt="" />
+                <img src={CorailGauche} id='corailGauche' style={{ position: 'absolute', left: '0', top: '27rem', height: '380px' }} alt="" />
+                <img src={CorailDroite} id='corailDroite' style={{ position: 'absolute', right: '0', top: '29rem' }} alt="" />
+                <img src={BigPoisson} id='bigPoisson' className='poissonGauche' style={{ position: 'absolute', top: '62rem', left: '14rem', height: '150px' }} alt="" />
+                <img src={SmallPoisson} id='smallPoisson' className='poissonDroite' style={{ position: 'absolute', right: '380px' }} alt="" />
+                <img src={SmallBulles} id='smallBulles' style={{ position: 'absolute', right: '10rem', top: '66rem' }} alt="" />
+                <img src={SmallPoisson} className='poissonDroite' style={{ position: 'absolute', top: '75rem', right: '18rem' }} alt="" />
+                <img src={Plongeur} style={{ position: 'absolute', right: '60px', top: '90rem' }} alt="" />
+                <div className='boiteDiscussion'>
+                    <p className='textDiscussion'>Discussion</p>
+                </div>
+            </div>   
+            < Quizz />  
+        </section>
+    )
+}
+
+
+export default Accueil;
--- frontend/src/components/Quizz.tsx
+++ frontend/src/components/Quizz.tsx
@@ -1,27 +1,97 @@
 import { FormEvent, useState } from "react";
 
 const Quizz = () => {
-  const data = [
-    {
-      question:
-        "Quel est le rôle des courants marins dans les océans, comparable à celui de quel système dans le corps humain ?",
-      réponse1: "Système immunitaire ",
-      réponse2: "Système digestif",
-      réponse3: "Système circulatoire",
-      réponse4: "Système nerveux",
-      vraieréponse: "réponse3",
-    },
-    {
-      question:
-        "Quelle est une conséquence de la perturbation des courants marins sur les écosystèmes océaniques ?",
-      réponse1: "Augmentation de la biodiversité",
-      réponse2: "Réchauffement des eaux de surface",
-      réponse3: "Stabilisation des températures globales",
-      réponse4: "Réduction des émissions de CO2",
-      vraieréponse: "réponse2",
-    },
+    const data = [
+      {
+        question: "Quelle est la couleur du cheval blanc d'Henri IV ?",
+        réponse1: "blanc",
+        réponse2: "jaune",
+        réponse3: "vert",
+        réponse4: "rouge",
+        vraieréponse: "réponse1",
+      },
+      {
+          question: "Quel est le rôle des courants marins dans les océans, comparable à celui de quel système dans le corps humain ?",
+          réponse1: "Système immunitaire ",
+          réponse2: "Système digestif",
+          réponse3: "Système circulatoire",
+          réponse4: "Système nerveux",
+          vraieréponse: "réponse3",
+      },
+      {
+          question: "Quelle est une conséquence de la perturbation des courants marins sur les écosystèmes océaniques ?",
+          réponse1: "Augmentation de la biodiversité",
+          réponse2: "Réchauffement des eaux de surface",
+          réponse3: "Stabilisation des températures globales",
+          réponse4: "Réduction des émissions de CO2",
+          vraieréponse: "réponse2",
+      },
+      {
+          question: "Les poumons humains sont comparables à quelle fonction océanique ?",
+          réponse1: "Courants marins",
+          réponse2: "Photosynthèse océanique",
+          réponse3: "Érosion côtière",
+          réponse4: "Décomposition marine",
+          vraieréponse: "réponse2",
+      },
+      {
+          question: "Quelles sont les conséquences de l'acidification des océans sur la photosynthèse océanique ?",
+          réponse1: "Déclin du phytoplancton",
+          réponse2: "Augmentation de l'absorption de CO2",
+          réponse3: "Stabilisation des niveaux d'oxygène",
+          réponse4: "Accélération de la production d'oxygène",
+          vraieréponse: "réponse1",
+      },
+      {
+          question: "Quelle partie du corps humain est comparable à la surface de l'océan en termes de protection et régulation thermique ?",
+          réponse1: "Coeur",
+          réponse2: "Foie",
+          réponse3: "Intestins",
+          réponse4: "Peau",
+          vraieréponse: "réponse4",
+      },
+      {
+          question: "Quels sont les effets potentiels du réchauffement de la surface océanique sur le climat mondial ?",
+          réponse1: "Augmentation des tempêtes et ouragans",
+          réponse2: "Stabilisation des températures",
+          réponse3: "Réduction des précipitations",
+          réponse4: "Augmentation de la production d'oxygène",
+          vraieréponse: "réponse1",
+      },
+      {
+          question: "Le phytoplancton situé dans les océans capture le CO2 et produit de l'oxygène, quel est l'organe du corps humains qui effectue l'action inverse ?",
+          réponse1: "Coeur",
+          réponse2: "Intestins",
+          réponse3: "Poumons",
+          réponse4: "Estomac",
+          vraieréponse: "réponse3",
+      },
+      {
+          question: "Quelles sont les conséquences de la dégradation des récifs coralliens sur les écosystèmes côtiers ?",
+          réponse1: "Protection accrue des côtes",
+          réponse2: "Érosion côtière accrue",
+          réponse3: "Amélioration de la biodiversité",
+          réponse4: "Stabilisation des niveaux de CO2",
+          vraieréponse: "réponse2",
+      },
+      {
+          question: "Comment la circulation des nutriments dans les océans est-elle comparable à une fonction humaine ?",
+          réponse1: "Elle protège le corps contre les infections",
+          réponse2: "Elle décompose la nourriture pour extraire les nutriments",
+          réponse3: "Elle transmet des signaux électriques",
+          réponse4: "Elle est similaire à la circulation du sang transportant les nutriments aux cellules",
+          vraieréponse: "réponse4",
+      },
+      {
+          question: "Quelle est l'importance de la décomposition marine pour l'écosystème océanique ?",
+          réponse1: "Production d'oxygène",
+          réponse2: "Filtration de l'eau",
+          réponse3: "Communication chimique",
+          réponse4: "Recyclage des nutriments",
+          vraieréponse: "réponse4",
+      },
 
-  ];
+];
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState("");
