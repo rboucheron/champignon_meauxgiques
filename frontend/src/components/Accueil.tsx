3b2c2fd3b37eeeeeb30f28aa6576b03a46c05593
Anthony Stanislas <103941632+Wixtil@users.noreply.github.com>
2024-12-06 04:41:30 +0100

    Ajout animation poisson

--- frontend/src/components/Accueil.tsx
+++ frontend/src/components/Accueil.tsx
@@ -6,7 +6,8 @@ import BigPoisson from '../assets/bigPoisson.png'
 import SmallPoisson from '../assets/smallPoisson.png'
 import SmallBulles from '../assets/smallBulles.png'
 import Plongeur from '../assets/plongeur.png'
-import { Animation } from './animation/Animation'
+import { AnimationPoissonGauche } from './animation/Animation'
+import { AnimationPoissonDroite } from './animation/Animation'
 
 
 const Accueil = () => {
@@ -18,14 +19,15 @@ const Accueil = () => {
                     <p>Lalala on est dans l'eau</p>
                 </div>
                 <img src={BullesGauche} id='bullesGauche' style={{ position: 'absolute', left: '60px', top: '350px' }} alt="" />
-                < Animation />
+                < AnimationPoissonGauche />
                 <img src={Hippocampes} id='hippocampesDroite' style={{ position: 'absolute', left: '37rem', top: '34rem', height: '60px' }} alt="" />
                 <img src={CorailGauche} id='corailGauche' style={{ position: 'absolute', left: '0', top: '27rem', height: '380px' }} alt="" />
                 <img src={CorailDroite} id='corailDroite' style={{ position: 'absolute', right: '0', top: '29rem' }} alt="" />
                 <img src={BigPoisson} id='bigPoisson' style={{ position: 'absolute', top: '62rem', left: '14rem', height: '150px' }} alt="" />
-                <img src={SmallPoisson} id='smallPoisson' style={{ position: 'absolute', right: '380px' }} alt="" />
+                <img src={SmallPoisson} id='smallPoisson' className="poissonDroite" style={{ position: 'absolute', right: '380px' }} alt="" />
+                < AnimationPoissonDroite/>
                 <img src={SmallBulles} id='smallBulles' style={{ position: 'absolute', right: '10rem', top: '66rem' }} alt="" />
-                <img src={SmallPoisson} className='smallPoisson' style={{ position: 'absolute', top: '75rem', right: '18rem' }} alt="" />
+                < AnimationPoissonDroite/>
                 <img src={Plongeur} style={{ position: 'absolute', right: '60px', top: '90rem' }} alt="" />
             </div>
         </section>
